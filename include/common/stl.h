2d4036d748378ee4eac0b247e3860aadbcc222a0
xezon <pascalkross@gmail.com>
2017-11-16 17:03:39 +0100

    Use stack buffer for string_format functions instead of heap allocations

--- include/common/stl.h
+++ include/common/stl.h
@@ -8,25 +8,35 @@
 
 namespace stl {
 
-template<typename... Args>
+template <size_t Size, class... Args>
+inline ::std::string string_format_n(const char* format, Args... args)
+{
+	char buf[Size];
+	::std::snprintf(buf, Size, format, args...);
+	return ::std::string(buf, buf + Size);
+}
+
+template <size_t Size, class... Args>
+inline ::std::wstring wstring_format_n(const wchar_t* format, Args... args)
+{
+	wchar_t buf[Size];
+	::std::swprintf(buf, Size, format, args...);
+	return ::std::wstring(buf, buf + Size);
+}
+
+template <class... Args>
 inline ::std::string string_format(const char* format, Args... args)
 {
-	size_t size = ::std::snprintf(nullptr, 0, format, args...) + 1;
-	::std::unique_ptr<char[]> buf(new char[size]);
-	::std::snprintf(buf.get(), size, format, args...);
-	return ::std::string(buf.get(), buf.get() + size - 1);
+	return string_format_n<1024>(format, args...);
 }
 
-template<typename... Args>
+template <class... Args>
 inline ::std::wstring wstring_format(const wchar_t* format, Args... args)
 {
-	size_t size = ::std::swprintf(nullptr, 0, format, args...) + 1;
-	::std::unique_ptr<wchar_t[]> buf(new wchar_t[size]);
-	::std::swprintf(buf.get(), size, format, args...);
-	return ::std::wstring(buf.get(), buf.get() + size - 1);
+	return wstring_format_n<1024>(format, args...);
 }
 
-template<class Container, class Value>
+template <class Container, class Value>
 inline bool find_and_erase(Container& container, const Value& value)
 {
 	typename Container::iterator it = ::std::find(container.begin(), container.end(), value);
@@ -38,7 +48,7 @@ inline bool find_and_erase(Container& container, const Value& value)
 	return false;
 }
 
-template<class Container, class Predicate>
+template <class Container, class Predicate>
 inline bool find_and_erase_if(Container& container, const Predicate& predicate)
 {
 	typename Container::iterator it = ::std::find_if(container.begin(), container.end(), predicate);
@@ -50,14 +60,14 @@ inline bool find_and_erase_if(Container& container, const Predicate& predicate)
 	return false;
 }
 
-template<class Iterator, class Value, class Compare = ::std::less<>>
+template <class Iterator, class Value, class Compare = ::std::less<>>
 inline Iterator binary_find(Iterator first, Iterator last, const Value& value, Compare comp = {})
 {
 	first = ::std::lower_bound(first, last, value, comp);
 	return (first != last) && !comp(value, *first) ? first : last;
 }
 
-template<class Iterator, class Value, class Compare = ::std::less<>>
+template <class Iterator, class Value, class Compare = ::std::less<>>
 inline typename Iterator::difference_type binary_find_index(Iterator first, Iterator last, const Value& value, Compare comp = {})
 {
 	Iterator it = binary_find(first, last, value, comp);
