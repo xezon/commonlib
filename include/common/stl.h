c7efd84ad32413ebdd3603d48fcb5c8841ce11ca
xezon <pascalkross@gmail.com>
2017-11-17 13:33:21 +0100

    Make string_format compatible with template strings

--- include/common/stl.h
+++ include/common/stl.h
@@ -5,35 +5,68 @@
 #include <cstdio>
 #include <memory>
 #include <algorithm>
+#include <exception>
 
 namespace stl {
+
+template <class... Args>
+inline int nprintf(char* buf, size_t count, const char* format, Args... args)
+{
+	return ::std::snprintf(buf, count, format, args...);
+}
+
+template <class... Args>
+inline int nprintf(wchar_t* buf, size_t count, const wchar_t* format, Args... args)
+{
+	return ::std::swprintf(buf, count, format, args...);
+}
+
+template <class String, size_t Size, class... Args>
+inline String string_format_tn(const typename String::allocator_type& allocator,
+                               const typename String::value_type* format, Args... args)
+{
+	typename String::value_type buf[Size];
+	const int n = ::stl::nprintf(buf, Size, format, args...);
+	if (n >= Size) {
+		throw ::std::length_error("Size is too small");
+	}
+	if (n < 0) {
+		throw ::std::invalid_argument("Encoding error occured");
+	}
+	String str(allocator);
+	str.assign(buf, buf + Size);
+	return str;
+}
+
+template <class String, class... Args>
+inline String string_format_t(const typename String::allocator_type& allocator,
+                              const typename String::value_type* format, Args... args)
+{
+	return string_format_tn<String, 1024>(allocator, format, args...);
+}
 
 template <size_t Size, class... Args>
 inline ::std::string string_format_n(const char* format, Args... args)
 {
-	char buf[Size];
-	::std::snprintf(buf, Size, format, args...);
-	return ::std::string(buf, buf + Size);
-}
-
-template <size_t Size, class... Args>
-inline ::std::wstring wstring_format_n(const wchar_t* format, Args... args)
-{
-	wchar_t buf[Size];
-	::std::swprintf(buf, Size, format, args...);
-	return ::std::wstring(buf, buf + Size);
+	return string_format_tn<::std::string, Size>(::std::string::allocator_type(), format, args...);
 }
 
 template <class... Args>
 inline ::std::string string_format(const char* format, Args... args)
 {
-	return string_format_n<1024>(format, args...);
+	return string_format_t<::std::string>(::std::string::allocator_type(), format, args...);
+}
+
+template <size_t Size, class... Args>
+inline ::std::wstring wstring_format_n(const wchar_t* format, Args... args)
+{
+	return string_format_tn<::std::wstring, Size>(::std::wstring::allocator_type(), format, args...);
 }
 
 template <class... Args>
 inline ::std::wstring wstring_format(const wchar_t* format, Args... args)
 {
-	return wstring_format_n<1024>(format, args...);
+	return string_format_t<::std::wstring>(::std::wstring::allocator_type(), format, args...);
 }
 
 template <class Container, class Value>
--- include/common/utils_log.h
+++ include/common/utils_log.h
@@ -6,26 +6,26 @@
 
 namespace utils {
 
-template<typename... Args>
-inline void LogA(const char* format, Args... args)
+template <class... Args>
+inline void clog(const char* format, Args... args)
 {
 	::std::clog << ::stl::string_format(format, args...).c_str() << ::std::endl;
 }
 
-template<typename... Args>
-inline void LogW(const wchar_t* format, Args... args)
+template <class... Args>
+inline void wclog(const wchar_t* format, Args... args)
 {
 	::std::wclog << ::stl::wstring_format(format, args...).c_str() << ::std::endl;
 }
 
-template<typename... Args>
-inline void ErrA(const char* format, Args... args)
+template <class... Args>
+inline void cerr(const char* format, Args... args)
 {
 	::std::cerr << ::stl::string_format(format, args...).c_str() << ::std::endl;
 }
 
-template<typename... Args>
-inline void ErrW(const wchar_t* format, Args... args)
+template <class... Args>
+inline void wcerr(const wchar_t* format, Args... args)
 {
 	::std::wcerr << ::stl::wstring_format(format, args...).c_str() << ::std::endl;
 }
