e884c5829143a5985921c6775aa4fdcad5ffcbdc
xezon <pascalkross@gmail.com>
2017-11-20 21:58:32 +0100

    Add types for MQL4

--- include/common/types.h
+++ include/common/types.h
@@ -10,6 +10,7 @@ typedef int            int32_t;
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int   uint32_t;
+typedef uint32_t       size_t;
 
 typedef struct sint64
 {
@@ -25,6 +26,22 @@ typedef struct uint64
 
 typedef void* alloc_func;
 typedef void* free_func;
+
+#else
+#ifdef MQL4
+
+#define int8_t   char
+#define int16_t  short
+#define int32_t  int
+#define int64_t  long
+#define uint32_t uint
+#define uint8_t  uchar
+#define uint16_t ushort
+#define uint64_t ulong
+#define size_t   uint32_t
+
+typedef void* (* alloc_func)(size_t count, size_t size);
+typedef void  (* free_func )(void* ptr, size_t count, size_t size);
 
 #else
 
@@ -43,8 +60,9 @@ typedef void  (__cdecl* free_func )(void* ptr, size_t count, size_t size);
 #include <gsl/pointers>
 typedef ::gsl::not_null<alloc_func> alloc_func_not_null;
 typedef ::gsl::not_null<free_func>  free_func_not_null;
-#endif
+#endif // __cplusplus
 
+#endif // MQL4
 #endif // ZORRO_LITE_C
 
 #endif // COMMON_TYPES_H_
