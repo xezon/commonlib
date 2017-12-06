49dfe851c089fa7ae2ef8b0ba798584a453fe503
xezon <pascalkross@gmail.com>
2017-12-06 13:27:06 +0100

    Add more utf 8 conversion function helpers

--- include/common/utf8.h
+++ include/common/utf8.h
@@ -18,6 +18,12 @@ inline ::std::u16string to_utf16(const char* str)
 	return c.from_bytes(str);
 }
 
+inline ::std::u16string to_utf16(const char* begin, const char* end)
+{
+	::std::wstring_convert<::std::codecvt_utf8_utf16<char16_t>, char16_t> c;
+	return c.from_bytes(begin, end);
+}
+
 inline ::std::wstring to_ucs2(const ::std::string& str)
 {
 	::std::wstring_convert<::std::codecvt_utf8<wchar_t>, wchar_t> c;
@@ -28,6 +34,12 @@ inline ::std::wstring to_ucs2(const char* str)
 {
 	::std::wstring_convert<::std::codecvt_utf8<wchar_t>, wchar_t> c;
 	return c.from_bytes(str);
+}
+
+inline ::std::wstring to_ucs2(const char* begin, const char* end)
+{
+	::std::wstring_convert<::std::codecvt_utf8<wchar_t>, wchar_t> c;
+	return c.from_bytes(begin, end);
 }
 
 inline ::std::string to_utf8(const ::std::u16string& str)
@@ -42,6 +54,12 @@ inline ::std::string to_utf8(const char16_t* str)
 	return c.to_bytes(str);
 }
 
+inline ::std::string to_utf8(const char16_t* begin, const char16_t* end)
+{
+	::std::wstring_convert<::std::codecvt_utf8_utf16<char16_t>, char16_t> c;
+	return c.to_bytes(begin, end);
+}
+
 inline ::std::string to_utf8(const ::std::wstring& str)
 {
 	::std::wstring_convert<::std::codecvt_utf8<wchar_t>, wchar_t> c;
@@ -52,6 +70,12 @@ inline ::std::string to_utf8(const wchar_t* str)
 {
 	::std::wstring_convert<::std::codecvt_utf8<wchar_t>, wchar_t> c;
 	return c.to_bytes(str);
+}
+
+inline ::std::string to_utf8(const wchar_t* begin, const wchar_t* end)
+{
+	::std::wstring_convert<::std::codecvt_utf8<wchar_t>, wchar_t> c;
+	return c.to_bytes(begin, end);
 }
 
 // Intentional string pass-through for platform dependent string types
